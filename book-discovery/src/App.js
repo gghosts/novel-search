1f333e538256b159d4aa949803eed1761e9d5491
tabi <tabiredder@live.com>
2021-08-24 22:31:31 -0500

    first commit

--- book-discovery/src/App.js
+++ book-discovery/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React, { Component } from "react";
+import Header from './components/Header'
+import "./App.css";
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
+      <Header/>
 
     </div>
-  );
+    );  
+}
 }
 
 export default App;
---
+++ book-discovery/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header>
+            <i class="fas fa-book-open"></i>
+            <h1>Novel Discovery Search</h1>
+        </header>
+    )
+}
+
+export default Header
